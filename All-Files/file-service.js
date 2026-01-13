b1d99ff9002bc306c4531c7ff8e2adfb3a3ce0f6
Sachin <sachinmizo1223@gmail.com>
2026-01-13 18:38:35 +0530

    Done some changes in addTaskk function

--- All-Files/file-service.js
+++ All-Files/file-service.js
@@ -2,6 +2,7 @@
 // Like  take the required input data from other file, if it is read, means just tos show the task list of our todos and write means adding new task to todo and deleting means delete the task from our json file.
 // These all acitons are triggered from task file and asked in this file to take again an actions.
 
+
 const fs = require('fs');
 const path = require('path');
 // By using path.join(), we create paths of all file into this file and we can perform operatiions in all the file from here by using (FILE SYSTEM)
@@ -25,22 +26,22 @@ function readTask(){
 
     }
 }
+
+
 //for write you will get the parameter from task file , as that thing only you have to write into task json file 
-function writeTask(data){
+function writeTask(NewTask){
 
     try{
-        fs.writeFileSync(datajson,JSON.stringify(data,null,2),"utf8")
+        fs.writeFileSync(datajson,JSON.stringify(NewTask,null,2),"utf8")
 
     }catch(error){
     //   No need to perform any sort of logic here if foud errro just sty silent 
     }
 
 }
-
 module.exports = {
     readTask,
     writeTask,
-
 }
 
 
--- All-Files/task-service.js
+++ All-Files/task-service.js
@@ -1,5 +1,12 @@
 // In this file we will write all the bussiness logic, of adding a task, updating a task , listing a task and deleting a task.
 
+// ORDER of functions which i am implementing 
+// 1)  addTask (simplest, confidence builder)
+// 2️⃣ listTasks
+// 3️⃣ markInProgress / markDone
+// 4️⃣ updateTask
+// 5️⃣ deleteTask
+
 const {STATUS,COLOR} = require('./constants')
 const {readTask , writeTask} = require('./file-service');
 
@@ -9,33 +16,42 @@ const {readTask , writeTask} = require('./file-service');
 // for adding task purpose we are going to use write task , which will call the function in our file service and from there we can add or write he task into our data.json file and update the file 
 // The logic to get new id everytime a new task is added is by definig a global counter which keeps track of the pervious id and the current id 
 
-const PreviousId = 0;
+
 function addTask(description){
        const currentTaskState = readTask();
        //here i wll get the list of all task and the last task of the list also 
-    const newId = PreviousId + 1;
+    const newId = currentTaskState.length()===0?1:currentTaskState[currentTaskState.length()- 1].id + 1;//means last task id , we get and then we append + 1 to that value like 10 + 1  = 11 which is now our new id value.
     // here we are creating the obejct of all the json data
     //from task service file we have to send the array to file servie file , writetask fuunction which will parse it into json and then write it in taskjson file 
 
     const CurrentTime = new Date().toISOString();
    
-        const data = {
+        const NewTask = {
         "id" : newId,
         "description":description ,
         "status" :STATUS.TODO,
         "color":COLOR.green,
-        "createdTime":now,
-        "updateTime":now,
-
-
+        "createdTime":CurrentTime,
+        "updateTime":CurrentTime,
     }
-
     currentTaskState.push(data);
     writeTask(currentTaskState);
 
+    return NewTask;
+}
+
+function ListTask(){
+
+    const ListAllTask = readTask();
 
 }
 
+
+
+
+function DeleteTask(){
+
+}
 module.exports = {
     data,
 }
